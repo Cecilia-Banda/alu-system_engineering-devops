78791bf11ca012301a519b289933c30c03e18205
Cecilia_Banda <105134294+Cecilia-Banda@users.noreply.github.com>
2022-12-09 18:49:47 +0200

    Update 0-strace_is_your_friend.pp

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 #Find out why Apache is giving an error
-exec {'The filename is not correct';
-  command => ' sed -i 's/phpp/php/g' /var/www/html/wp-settings.php' ,
+exec {'The filename is not correct':
+  command => ' sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php' ,
   provider => 'shell'
 }
